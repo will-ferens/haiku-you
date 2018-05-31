db3456cfb494a94676bf03c49a4f263c552abc77
_dream <will.ferens@gmail.com>
2018-05-31 15:40:58 -0600

    launch

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -7,19 +7,19 @@ const Footer = () => {
             <ul className="links" >
                 <li> <small>Written by @will_ferens</small></li>
                 <li className="nav-item">
-                    <a href="https://twitter.com/will_ferens" target="_blank">
+                    <a href="https://twitter.com/will_ferens" target="_blank" rel="noopener noreferrer">
                     <svg width="17" height="17" viewBox="0 0 1024 1024" >
                         <path d={ICONS.TWITTER}></path>
                     </svg>
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a href="https://github.com/will-ferens" target="_blank"><svg width="17" height="17" viewBox="0 0 1024 1024">
+                    <a href="https://github.com/will-ferens" target="_blank" rel="noopener noreferrer"><svg width="17" height="17" viewBox="0 0 1024 1024">
                     <path d={ICONS.GITHUB}></path>
                     </svg></a>
                 </li>
                 <li className="nav-item">
-                    <a href="https://www.linkedin.com/in/will-ferens/" target="_blank">
+                    <a href="https://www.linkedin.com/in/will-ferens/" target="_blank" rel="noopener noreferrer">
                     <svg width="22" height="22" viewBox="0 0 1024 1024">
                         <path d={ICONS.LINKEDIN}></path>
                     </svg>
--- app/src/components/TwitterStream.js
+++ app/src/components/TwitterStream.js
@@ -12,7 +12,8 @@ class TwitterStream extends Component {
                 <a className="twitter-timeline"
                 href="https://twitter.com/haiku_you19"
                 data-chrome="nofooter noborders"
-                data-height="400">
+                data-height="400"
+                border-color="#A8AAFF">
                 </a>
             </div>
         )  
